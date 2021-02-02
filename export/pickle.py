a36d4612b705af497a15a70c2e48bce3aa94bc8c
Luxii <dl94@live.de>
2021-02-02 14:54:17 +0100

    Small change in pickle export

--- export/pickle.py
+++ export/pickle.py
@@ -16,7 +16,7 @@ class Pickle(AExporter):
         accession = prot_graph.vs["accession"][0]
         out_dir = os.path.join(self.out_folder, *[x for x in accession[:-1]])
         if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
+            os.makedirs(out_dir, exist_ok=True)
         prot_graph.write_pickle(os.path.join(out_dir, accession[-1:] + ".pickle"))
 
     def tear_down(self):
