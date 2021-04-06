5c7829bb88392371773cb490156f354fff0c1ad5
Luxii <dl94@live.de>
2021-04-06 19:26:29 +0200

    Linting of Bash-Script

--- utilities/citus_set_db_and_bit.sh
+++ utilities/citus_set_db_and_bit.sh
@@ -2,9 +2,9 @@
 # The password for the postgres user
 CLUSTER_PWD="<Password>" 
 # All postgresql machines with citus
-CLUSTER_MACHINES=("192.168.0.3", "192.168.0.4", "192.168.0.5")
+CLUSTER_MACHINES=("192.168.0.3" "192.168.0.4" "192.168.0.5")
 # node names (shards)
-CLUSTER_NODES=(worker-01, worker-02)
+CLUSTER_NODES=(worker-01 worker-02)
 CLUSTER_CONTROLLER="192.168.0.3"
 
 # Set here the database name
@@ -14,10 +14,10 @@ create_database(){
     # Create database on controller and nodes (if not exists) and install citus extension
     for cluster_machine in "${CLUSTER_MACHINES[@]}"
     do
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres <<-EOSQL
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres <<-EOSQL
             SELECT 'CREATE DATABASE ${DB_NAME}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}')\\gexec
 EOSQL
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -d ${DB_NAME} -c "CREATE EXTENSION IF NOT EXISTS citus;"
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -d ${DB_NAME} -c "CREATE EXTENSION IF NOT EXISTS citus;"
     done
 
     # Add nodes to controller
@@ -35,14 +35,14 @@ create_bits(){
     # Create bit hash function for distributing
     for cluster_machine in "${CLUSTER_MACHINES[@]}"
     do
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
             CREATE FUNCTION equal_test_bit_function(bit(452), bit(452)) RETURNS boolean
             AS 'select \$1 = \$2;'
             LANGUAGE SQL
             IMMUTABLE
             RETURNS NULL ON NULL INPUT;
 EOSQL
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
             CREATE OPERATOR = (
                 LEFTARG = bit(452),
                 RIGHTARG = bit(452),
@@ -50,20 +50,20 @@ EOSQL
                 HASHES
             );
 EOSQL
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
             CREATE FUNCTION bit_hash(bit(452)) RETURNS int
             AS 'SELECT hashtext(\$1::text);'   
             LANGUAGE SQL
             IMMUTABLE
             RETURNS NULL ON NULL INPUT;
 EOSQL
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
             CREATE OPERATOR CLASS new_op_fam_hash_class
             DEFAULT FOR TYPE bit(452) USING HASH AS
             OPERATOR 1 = (bit(452), bit(452)),
             FUNCTION 1 bit_hash(bit(452));
 EOSQL
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -d ${DB_NAME} <<-EOSQL
             CREATE OPERATOR CLASS new_op_fam_btree_class
             DEFAULT FOR TYPE bit(452) USING BTREE AS
             OPERATOR 3 = (bit(452), bit(452));
@@ -76,10 +76,10 @@ drop_database (){
     # Also remove all functions which may have been generated
     for cluster_machine in "${CLUSTER_MACHINES[@]}"
     do
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -c "DROP DATABASE IF EXISTS ${DB_NAME};"
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -c "DROP operator class new_op_fam_hash_class using hash;"
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -c "DROP function bit_hash;"
-        PGPASSWORD=${CLUSTER_PWD} psql -h $cluster_machine -p 5432 -U postgres -c "drop function equal_test_bit_function cascade;"
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -c "DROP DATABASE IF EXISTS ${DB_NAME};"
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -c "DROP operator class new_op_fam_hash_class using hash;"
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -c "DROP function bit_hash;"
+        PGPASSWORD=${CLUSTER_PWD} psql -h "$cluster_machine" -p 5432 -U postgres -c "drop function equal_test_bit_function cascade;"
     done
 }
 
