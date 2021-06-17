ce50f0e7581e12991a7b270776d472c7424a77eb
Luxii <dl94@live.de>
2021-06-17 15:43:47 +0200

    New verify functions for the protein graphs

--- protgraph/verify_graphs.py
+++ protgraph/verify_graphs.py
@@ -4,6 +4,39 @@ def verify_graph(graph):
 
     _check_parallel_edges(graph)
 
+    # Check degrees!
+    _check_degree(graph)
+    _check_indegree(graph)
+    _check_outdegree(graph)
+
+
+def _check_degree(graph):
+    if len([x for x in graph.degree() if x == 0]) != 0:
+        print(
+            "Degree of Protein {} is for some vertices set at 0! "
+            "There might be some not connected Subgraphs!".format(graph.vs[0]["accession"])
+        )
+
+def _check_indegree(graph):
+    if len([x for x in graph.indegree() if x == 0]) != 1:
+        print(
+            "InDegree of Protein {} is not 1 (actual {})! "
+            "There might be some not connected Subgraphs!".format(
+                graph.vs[0]["accession"],
+                len([x for x in graph.indegree() if x == 0])
+            )
+        )
+
+def _check_outdegree(graph):
+    if len([x for x in graph.outdegree() if x == 0]) != 1:
+        print(
+            "OutDegree of Protein {} is not 1 (actual {})! "
+            "There might be some not connected Subgraphs!".format(
+                graph.vs[0]["accession"],
+                len([x for x in graph.outdegree() if x == 0])
+            )
+        )
+
 
 def _check_dag(graph):
     if not graph.is_dag:
