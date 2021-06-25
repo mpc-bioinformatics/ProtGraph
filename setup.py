aa06e2c764cb783c6b89d4f9f2e567b2e8e233af
Luxii <dl94@live.de>
2021-06-25 15:27:53 +0200

    Version Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='protgraph',
-    version='0.1.2',
+    version='0.2.0',
     author="Dominik Lux",
     description="ProtGraph, a graph generator for proteins.",
     long_description=long_description,
